5d442b3f64873bf00fca9259ff25b3a6a79fdc10
mahdi <mahdikiany@gmail.com>
2018-08-19 11:33:11 +0430

    better colors

--- Game/scripts/scripts.js
+++ Game/scripts/scripts.js
@@ -2,12 +2,12 @@ var now = new Date();
 var start = now.getTime();
 
 
-var BetweenStimuliTime = 300;    
+var BetweenStimuliTime = 300;
 var p = 0.5;
 
 var best = 100;
 var worst = 0;
-var data = []; 
+var data = [];
 var seq;
 
 var rand;
@@ -15,28 +15,26 @@ var rand;
 function makeShapeAppear() {
     rand = Math.random();
 
-    if (rand > p){
-	    var topOffset = 70;
-	    var leftOffset = 30;
-	    var red = 256;
-	    var blue = 0;
-	    var size =  100;
-            seq = 0;
-     }
+    if (rand > p) {
+        var topOffset = 70;
+        var leftOffset = 30;
+        var bgcolor = '#D81C2B';
+        var size = 100;
+        seq = 0;
+    }
 
-    if (rand < p){
-	    var topOffset = 70;
-	    var leftOffset = 70;
-	    var red = 0;
-	    var blue = 256;
-	    var size =  100;
-            seq = 1;
-     }
+    if (rand < p) {
+        var topOffset = 70;
+        var leftOffset = 70;
+        var bgcolor = '#88CCF1';
+        var size = 100;
+        seq = 1;
+    }
 
     document.getElementById('shape').style.borderRadius = "50%";
     document.getElementById('shape').style.top = topOffset + "%";
-    document.getElementById('shape').style.left = leftOffset +"%";
-    document.getElementById('shape').style.backgroundColor = "rgb(" + red + ", " + 0 + ", " + blue + ")";
+    document.getElementById('shape').style.left = leftOffset + "%";
+    document.getElementById('shape').style.backgroundColor = bgcolor;
     document.getElementById('shape').style.width = size + "px";
     document.getElementById('shape').style.height = size + "px";
     document.getElementById('shape').style.display = 'block';
@@ -53,20 +51,19 @@ appearAfterDelay();
 
 
 // Keyboard Input Handle
-document.addEventListener('keydown', function(event) {
-    if(event.keyCode == 37) {
-	if(rand>p){
-	    endstimuli();
-	}
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode == 37) {
+        if (rand > p) {
+            endstimuli();
+        }
+    } else if (event.keyCode == 39) {
+        if (rand < p) {
+            endstimuli();
+        }
     }
-    else if(event.keyCode == 39) {
-	if(rand<p){
-	    endstimuli();
-	}
-    }
-    if (event.keyCode == 27){
-	window.alert(data);
-	window.open("https://hband.ir/Neurogame/get.php?time="+ data)
+    if (event.keyCode == 27) {
+        window.alert(data);
+        window.open("https://hband.ir/Neurogame/get.php?time=" + data)
     }
 });
 
@@ -78,22 +75,22 @@ document.getElementById('shape').onclick = function () {
 
 
 // Update after correct input
-function endstimuli(){
+function endstimuli() {
 
     document.getElementById('shape').style.display = 'none';
 
     var end = new Date().getTime(),
         timeTaken = (end - start) / 1000;
 
-        if (timeTaken > worst) {
-            worst = timeTaken;
-        }
+    if (timeTaken > worst) {
+        worst = timeTaken;
+    }
 
-        if (timeTaken < best) {
-            best = timeTaken;
-        }
+    if (timeTaken < best) {
+        best = timeTaken;
+    }
 
-    data.push([seq,timeTaken]); //Save results
+    data.push([seq, timeTaken]); //Save results
     document.getElementById('timeTaken').innerHTML = timeTaken + 's';
     document.getElementById('timeBest').innerHTML = best + 's';
     document.getElementById('timeWorst').innerHTML = worst + 's';
@@ -101,19 +98,20 @@ function endstimuli(){
     appearAfterDelay();
 }
 
-function right(){
-	if(rand<p){
-	    endstimuli();
-	}
- 
+function right() {
+    if (rand < p) {
+        endstimuli();
+    }
+
 }
-function left(){
-	if(rand>p){
-	    endstimuli();
-	}
+
+function left() {
+    if (rand > p) {
+        endstimuli();
+    }
 }
- 
-function end(){
-	window.alert(data);
-	window.open("https://hband.ir/Neurogame/get.php?time="+ data)
+
+function end() {
+    window.alert(data);
+    window.open("https://hband.ir/Neurogame/get.php?time=" + data)
 }
