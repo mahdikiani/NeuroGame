afe555ace8b4da8fd869cb1d810449a2fbc25d63
mahdi <mahdikiany@gmail.com>
2018-08-21 19:24:58 +0430

    fin

--- Game/get.php
+++ Game/get.php
@@ -1,8 +1,9 @@
 <?php
-$file = '../content.txt';
-// Open the file to get existing content
-$current = file_get_contents($file);
-// Append a new person to the file
-$current .= json_encode($_GET) . "\n";
-// Write the contents back to the file                                          file_put_contents($file, $current);
+    $file = '../content.txt';
+    // Open the file to get existing content
+    $current = file_get_contents($file);
+    // Append a new person to the file
+    $current .= json_encode($_GET) . "\n";
+    // Write the contents back to the file  
+    file_put_contents($file, $current);
 ?>
